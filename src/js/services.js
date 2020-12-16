84e8f381cf89cdec2300a0f42dc04829a4a4070e
Roman Burachevskij <burachevskijroman@gmail.com>
2020-12-17 02:25:19 +0300

    Services Page

--- src/js/services.js
+++ src/js/services.js
@@ -24,8 +24,27 @@ $(document).ready(function() {
   $('.tab').on("click", function() {
     var $index = $(this).index();
     for (let i = 0; i < $('.content-block').length; i++) {
-      $('.content-block').eq(i).css('display', 'none');
-      $('.content-block').eq($index).css('display', 'block');
+      $('.content-block').eq(i).css({
+        'display': 'none', 
+        'opacity': 0
+      });
+      $('.content-block').eq($index).css({
+        'display': 'block', 
+        'opacity': 1
+      });
+    }
+  })
+
+  // Questions
+  $('.question-button').on("click", function() {
+    if ( $(this).next().css('display') == 'none' ) {
+      $(this).addClass('openedQuestion');
+      $(this).removeClass('closedQuestion');
+      $(this).next().slideDown(300);
+    } else {
+      $(this).addClass('closedQuestion');
+      $(this).removeClass('openedQuestion');
+      $(this).next().slideUp(300);
     }
   })
 })
